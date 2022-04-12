0af3bbbfc6d4f5d98d5f2910694853ad02a71876
srikanth <srikanth.vangari@sonarsource.com>
2022-04-12 13:21:09 +0200

    new file added

--- src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
+++ src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
@@ -35,23 +35,3 @@ public class MyAnalysis {
         });
     }
 }
-        for(int i=0; i > 200; i++)
-
-        {
-            System.out.println();
-
-        }
-
-        for(int i=0; i > 200; i++)
-
-        {
-            System.out.println();
-
-        }
-
-        for(int i=0; i > 200; i++)
-
-        {
-            System.out.println();
-
-        }
