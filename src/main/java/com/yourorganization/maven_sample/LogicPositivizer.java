e00fb5cf5a212993ff178c43e34e570fffad9974
srikanth <srikanth.vangari@sonarsource.com>
2022-04-13 12:41:21 +0200

    change in code

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -50,6 +50,24 @@ public class LogicPositivizer {
             System.out.println();
 
         }
+        for(int i=0; i > 200; i++)
+
+        {
+            System.out.println();
+
+        }
+        for(int i=0; i > 200; i++)
+
+        {
+            System.out.println();
+
+        }
+        for(int i=0; i > 200; i++)
+
+        {
+            System.out.println();
+
+        }
 
 
         Log.info("Positivizing!");
